27582f08fdfdd50d9f8be9ad6460a6074a99aa2f
spector700 <72362399+spector700@users.noreply.github.com>
2024-10-05 16:51:06 -0500

    fix: change port for deployment

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/", indexRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT, () =>
   console.log(`My first Express app - listening on port ${PORT}!`),
 );
